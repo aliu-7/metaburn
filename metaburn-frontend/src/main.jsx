031b5cef6c3a6577a8d36941945cee256244631f
Alex Liu <alexliu@alexs-mbp.dhcp.nd.edu>
2025-04-11 11:12:13 -0400

    Finalize working frontend + backend with Tailwind setup

--- metaburn-frontend/src/main.jsx
+++ metaburn-frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './output.css'
+import "./index.css";  // Tailwind styles
+// import './output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
